9dbbd59099fcde0767ccac406307180cc640ef9f
gery25 <gerymaps200515@gmail.com>
2025-04-15 10:09:01 +0200

    add states machine

---
+++ src/xarxes2025/state_machine.py
@@ -0,0 +1,21 @@
+class state_machine :
+    def __init__(self):
+        #estat inicial
+        self.state = "INIT"
+
+        # Trancicions possibles
+        self.transitions = {
+            'INIT': {'SETUP': 'READY'},
+            'READY': {'PLAY': 'PLAYING', 'TEARDOWN': 'INIT'},
+            'PLAYING': {'PAUSE': 'READY', 'TEARDOWN': 'INIT'}
+        }
+    
+    def transition(self, event):
+        if event in self.transitions[self.state]:
+            self.state = self.transitions[self.state][event]
+            return True
+        else:
+            return False
+        
+    def get_state(self):
+        return self.state
